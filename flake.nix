732a4b6cf977101e388ac7dfc39585e6e65af6b8
Tommi Virtanen <tv@eagain.net>
2022-05-03 16:30:40 -0600

    nix: Specify rust version, make rust-analyzer happy

--- flake.nix
+++ flake.nix
@@ -1,35 +1,69 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = github:oxalica/rust-overlay;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    gitignore = {
+      url = github:hercules-ci/gitignore;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk, gitignore }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        naersk-lib = naersk.lib."${system}";
+        rustVersionOverlay = (self: prev:
+          let
+            rustChannel = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          in
+          {
+            rustc = rustChannel;
+            cargo = rustChannel;
+          }
+        );
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlay
+            rustVersionOverlay
+          ];
+        };
+        naersk-lib = naersk.lib.${system}.override {
+          rustc = pkgs.rustc;
+          cargo = pkgs.cargo;
+        };
+        gitignoreSource = gitignore.outputs.lib.gitignoreSource;
       in
       rec {
         # `nix build`
         packages.wait-for-pid = naersk-lib.buildPackage {
           pname = "wait-for-pid";
-          root = ./.;
+          # Avoid ingesting all of `target/` into the Nix store.
+          src = gitignoreSource ./.;
         };
         defaultPackage = packages.wait-for-pid;
 
         # `nix run`
-        apps.wait-for-pid = utils.lib.mkApp {
+        apps.wait-for-pid = flake-utils.lib.mkApp {
           drv = packages.wait-for-pid;
         };
         defaultApp = apps.wait-for-pid;
 
         # `nix develop`
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
         };
       });
 }
